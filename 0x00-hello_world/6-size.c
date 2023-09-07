10ea1f94630587e705080fa148579bafda956a3e
mTungatheone <imtunga13@gmail.com>
2023-09-07 05:31:19 -0700

    low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of double: %d bytes\n", sizeof(double));
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of float: %d bytes\n", sizeof(float));
+printf("Size of char: %d byte\n", sizeof(char));
 return (0);
 }
