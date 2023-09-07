9c01ea014be11c57484f5ba4717d9672462970e1
mTungatheone <imtunga13@gmail.com>
2023-09-07 04:50:22 -0700

    low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\n"Programming is like building a multilingual puzzle");
-}
+puts("\n"Programming is like building a multilingual puzzle");
 return (0);
+}
